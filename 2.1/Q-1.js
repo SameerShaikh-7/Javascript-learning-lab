9db6c9865f04fdacd6fcd629c50ff3c6d9508090
SameerShaikh-7 <shaikh.sameer.76220@gmail.com>
2025-12-05 21:28:10 +0530

    Add files via upload

---
+++ 2.1/Q-1.js
@@ -0,0 +1,7 @@
+let name = "Your Name";
+let age = 18;
+let school = "Your School";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("School: " + school);
---
+++ 2.1/Q-2.js
@@ -0,0 +1,7 @@
+console.log("- - - - - - - -");
+console.log("|              |");
+console.log("R              |");
+console.log("N              |");
+console.log("W              |");
+console.log("|              |");
+console.log("- - - - - - - -");
